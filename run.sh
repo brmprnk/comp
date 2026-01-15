51f6d98f4233fcfe0489f037e0449531c212f923
lnavarcikova <lucka.navarcikova@gmail.com>
2026-01-15 13:07:01 +0100

    script for all bin features

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #SBATCH --account=ewi-insy-prb
 #SBATCH --partition=insy,general # Request partition. Default is 'general'
-#SBATCH --job-name=wps-test
+#SBATCH --job-name=bin-all
 #SBATCH --qos=short        # Request Quality of Service. Default is 'short' (maximum run time: 4 hours)
-#SBATCH --time=3:00:00      # Request run time (wall-clock). Default is 1 minute
-#SBATCH --cpus-per-task=1   # Request number of CPUs (threads) per task. Default is 1 (note: CPUs are always allocated to jobs per 2).
-#SBATCH --mem=8GB         # Request memory (MB) per node. Default is 1024MB (1GB). For multiple tasks, specify --mem-per-cpu instead
+#SBATCH --time=5:00:00      # Request run time (wall-clock). Default is 1 minute
+#SBATCH --cpus-per-task=64   # Request number of CPUs (threads) per task. Default is 1 (note: CPUs are always allocated to jobs per 2).
+#SBATCH --mem=16GB         # Request memory (MB) per node. Default is 1024MB (1GB). For multiple tasks, specify --mem-per-cpu instead
 #SBATCH --mail-type=END     # Set mail type to 'END' to receive a mail when the job finishes.
 #SBATCH --output=%j_all.out # Set name of output log. %j is the Slurm jobId
 #SBATCH --error=%j_all.out # Set name of error log. %j is the Slurm jobId
@@ -14,7 +14,7 @@
 
 # Build the command with optional job splitting parameters
 # CMD_ARGS="-I input/cris_bam.txt --gc_file input/cris_gc.txt -c 64 -F BIN -b hg38_extended_chrom1.bed"
-CMD_ARGS="-I cris/EE87922.hg38.frag.tsv.bam --gc_file GC/Output_Bam/EE87922.hg38.frag.tsv__correction_factors.csv -c 1 -F WPS -b beds/hg38_extended_chrom1.bed"
+CMD_ARGS="-I input/cris_all.txt --gc_file input/cris_all_gc.txt -c 64 -F BIN -b beds/genome_bins/hg38_1mb_filtered_bins.bed"
 # CMD_ARGS="-I input/cris_bam.txt --gc_file input/cris_gc.txt -c 64 -F BIN -b beds/biomart_10kb.bed --aggregate"  # Add this if you want to aggregate coverage across loci (.npy)
 # CMD_ARGS="-I input/cris_bam.txt --gc_file input/cris_gc.txt -c 64 -F BIN -b beds/biomart_10kb.bed --coverage"  # Add this if you want to store coverage for each locus (.npy), but be cautious of large file sizes, especially with many loci!!!
 
